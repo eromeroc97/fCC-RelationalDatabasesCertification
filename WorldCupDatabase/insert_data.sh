be39af5f3d4fc411ea7fecff071ac7e7d39e039d
eromeroc97 <e.romerociudad@outlook.com>
2024-02-13 16:00:20 +0100

    Update insert_data.sh

--- WorldCupDatabase/insert_data.sh
+++ WorldCupDatabase/insert_data.sh
@@ -8,11 +8,16 @@ fi
 
 # No cambies el código arriba de esta línea. Usa la variable PSQL para consultar tu base de datos.
 tail -n +2 games.csv | awk -F ',' '{print $3 "\n" $4}' | sort | uniq | while read -r team; do
-    echo "$(psql -c "INSERT INTO teams (name) VALUES ('$team');")"
+    echo "$($PSQL "INSERT INTO teams (name) VALUES ('$team');")"
 done
 
 while IFS=',' read -r year round winner opponent winner_goals opponent_goals; do
-    winner_id=$(psql -Atc "SELECT id FROM teams WHERE name = '$winner'")
-    opponent_id=$(psql -Atc "SELECT id FROM teams WHERE name = '$opponent'")
-    echo "$(psql -c "INSERT INTO games (year, round, winner_id, opponent_id, winner_goals, opponent_goals) VALUES ('$year', '$round', '$winner_id', '$opponent_id', '$winner_goals', '$opponent_goals');")"
-done
+    echo "Processing: year=$year, round=$round, winner=$winner, opponent=$opponent, winner_goals=$winner_goals, opponent_goals=$opponent_goals"
+    if [ -n "$winner" ] && [ -n "$opponent" ]; then
+        winner_id="$($PSQL "SELECT team_id FROM teams WHERE name = '$winner'")"
+        opponent_id="$($PSQL "SELECT team_id FROM teams WHERE name = '$opponent'")"
+        echo "$($PSQL "INSERT INTO games (year, round, winner_id, opponent_id, winner_goals, opponent_goals) VALUES ('$year', '$round', '$winner_id', '$opponent_id', '$winner_goals', '$opponent_goals')")"
+    else
+        echo "Error: Empty values detected for winner or opponent."
+    fi
+done < games.csv
